e5a6c0a2be7dda4ea129f76b797ecb9818882620
benhurst1 <ben-hurst@outlook.com>
2024-01-03 11:33:36 +0000

    pull data dag complete

---
+++ dags/pull_data_dag.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta, date
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+import logging
+
+# import os
+import boto3
+import requests
+import json
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+dag = DAG(
+    "pull_predictit_data",
+    default_args=default_args,
+    description="Pull predictit data and store in s3 bucket",
+    schedule_interval="0 0 * * *",
+    start_date=datetime(2024, 1, 4),
+    catchup=False,
+)
+
+
+def request_json():
+    logging.info("Started task")
+
+    aws_access = Variable.get("AWS_ACCESS_KEY")
+    aws_secret_access = Variable.get("AWS_SECRET_ACCESS_KEY")
+    bucket = Variable.get("BUCKET")
+
+    filename = f"predictit-data-{date.today()}"
+    s3 = boto3.client(
+        "s3", aws_access_key_id=aws_access, aws_secret_access_key=aws_secret_access
+    )
+
+    logging.info("Connecting")
+    try:
+        response = requests.get("https://www.predictit.org/api/marketdata/all/")
+        logging.info("Connected")
+    except:
+        logging.info("Connection failed")
+
+    data = response.json()
+    s3.put_object(Body=str(json.dumps(data)), Bucket=bucket, Key=f"data/{filename}")
+
+
+pull_data_task = PythonOperator(
+    task_id="pull_data_task",
+    python_callable=request_json,
+    dag=dag,
+)
+
+pull_data_task
